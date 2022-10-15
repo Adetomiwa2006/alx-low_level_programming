deceeb2dd59b3d31e555ab4cc35ad2c8d85e72a6
Aladejare Adetomiwa <adetomiwa0103@gmail.com>
2022-10-15 09:17:55 -0700

    A very great commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+/**
+ * main -using the printf function
+ *
+ * Return: 0
+*/
 
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
